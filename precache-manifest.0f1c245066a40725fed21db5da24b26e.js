1e876b7cbbfd3b79c2969abcff82c5c6d73148e3
360BUYAD\liyang86 <liyang86@jd.com>
2019-11-04 16:00:47 +0800

    test

---
+++ precache-manifest.0f1c245066a40725fed21db5da24b26e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5751a4e4f0d177b785c3bb9370b47dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199fabce49ea8c6cae16",
+    "url": "/static/css/main.b1fe2347.chunk.css"
+  },
+  {
+    "revision": "05745221259cc0b839be",
+    "url": "/static/js/2.0a430a9b.chunk.js"
+  },
+  {
+    "revision": "199fabce49ea8c6cae16",
+    "url": "/static/js/main.cc4dd494.chunk.js"
+  },
+  {
+    "revision": "19dffa7a1e78686fab4a",
+    "url": "/static/js/runtime-main.ca086102.js"
+  }
+]);
